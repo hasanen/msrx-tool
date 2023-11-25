5ac65f647f67321da7c87da19a54781ef3543785
Joni Hasanen <joni@pieceofcode.com>
2023-11-25 16:17:01 +0200

    Detect if device is connected or not

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use rusb::{Context, Device, DeviceHandle, DeviceList, UsbContext};
+use std::process;
+
+const MSRX6: [u16; 2] = [0x0801, 0x0003];
+
 fn main() {
-    println!("Hello, world!");
+    // Initialize a USB context
+    let context = Context::new().expect("Failed to initialize USB context");
+
+    let device = match context.open_device_with_vid_pid(MSRX6[0], MSRX6[1]) {
+        Some(device) => device,
+        None => {
+            println!("Device not found");
+            process::exit(1)
+        }
+    };
+
+    dbg!(device);
 }
